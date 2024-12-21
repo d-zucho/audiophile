3a77b6f540ebd4088016a6d7328ea72052848d1d
d-zucho <danny.nico.dev@outlook.com>
2024-12-21 15:40:13 -0800

    start mobile nav

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,10 +5,11 @@ import Menu from '@/assets/shared/tablet/icon-hamburger.svg'
 
 const Navbar = () => {
   return (
-    <header className='bg-my-dark flex items-center justify-end  lg:pb-9 px-6 w-full overflow-hidden'>
+    <header className='bg-my-dark flex items-center justify-end mx-auto  px-2 sm:px-6 md:px-10 w-full overflow-hidden container'>
       <div className='flex items-center justify-between w-full border-b border-white/10 py-8 lg:pb-9'>
-        <Image src={Menu} alt='menu' />
-        <Image src={Logo} alt='Audiophile' />
+        <Image src={Menu} alt='menu' className='lg:hidden md:mr-10' />
+        <Image src={Logo} alt='Audiophile' className='md:mr-auto' />
+
         <Image src={Cart} alt='cart' />
       </div>
     </header>
---
+++ src/lib/data.ts
@@ -0,0 +1,6 @@
+const NAV_LINKS: NavLink[] = [
+  { href: '/', label: 'Home' },
+  { href: '/headphones', label: 'Headphones' },
+  { href: '/speakers', label: 'Speakers' },
+  { href: '/earphones', label: 'Earphones' },
+]
